93d5e78699fb489f3acc79470e1befa725dd1814
Alberto Ginel <aginelsalvador@gmail.com>
2024-07-19 01:16:03 +0300

    CORS

--- src/ProductController.php
+++ src/ProductController.php
@@ -107,9 +107,6 @@ private function processCollectionRequest(string $method): void
 
     case "DELETE":
 
-
-      echo json_encode(["In delete" => $method]);
-
       $data = file_get_contents("php://input", true);
       $dataJSON = json_decode($data, true);
 
