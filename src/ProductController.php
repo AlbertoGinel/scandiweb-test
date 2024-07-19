de5dace855b2ceb7fbffcb2c721d86c0b00b8dc9
Alberto Ginel <aginelsalvador@gmail.com>
2024-07-19 21:06:38 +0300

    CORS

--- src/ProductController.php
+++ src/ProductController.php
@@ -129,19 +129,15 @@ class ProductController
 
                 if (isset($dataJSON['idList'])) {
 
-                    http_response_code(418);
-
                     if (!isset($dataJSON['idList']) || !is_array($dataJSON['idList'])) {
                         http_response_code(400);
                         echo json_encode(["error" => "Invalid input, expected 'idList' as an array"]);
                         break;
                     }
 
-                    $idList = $dataJSON['idList'];
+                    $idList = [72]; //$dataJSON['idList'];
                     $deletedIds = [];
                     $errors = [];
-
-                    echo json_encode(["idList" => $idList ]);
 
                     foreach ($idList as $id) {
                         if ($this->gateway->delete($id)) {
