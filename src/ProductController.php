19745bf1d096f0583481d0689ab6d298ecb31a8d
Alberto Ginel <aginelsalvador@gmail.com>
2024-07-19 04:20:12 +0300

    CORS

--- src/ProductController.php
+++ src/ProductController.php
@@ -100,7 +100,7 @@ private function processCollectionRequest(string $method): void
         exit;
         break;
 
-    case "DELETE":
+    case "HEAD":
 
       //echo json_encode(["donde?" => "entramos en delete!"]);
 
@@ -143,8 +143,6 @@ private function processCollectionRequest(string $method): void
         $data = file_get_contents("php://input", true);
         $dataJSON = (array) json_decode($data);
 
-        if (!array_key_exists('idList', $dataJSON)) {
-
           $newProduct = $this->gateway->overLoadConstructor($dataJSON);
           $errors = $newProduct->getValidationErrors();
 
@@ -162,37 +160,6 @@ private function processCollectionRequest(string $method): void
               "id" => $id
           ]);
           break;
-        }else{
-          if (!isset($dataJSON['idList']) || !is_array($dataJSON['idList'])) {
-            http_response_code(400);
-            echo json_encode(["error" => "Invalid input, expected 'idList' as an array"]);
-            break;
-        }
-  
-        $idList = $dataJSON['idList'];
-        $deletedIds = [];
-        $errors = [];
-  
-        foreach ($idList as $id) {
-            if ($this->gateway->delete($id)) {
-                $deletedIds[] = $id;
-            } else {
-                $errors[] = "Failed to delete ID $id";
-          }
-        }
-  
-        if (!empty($errors)) {
-            http_response_code(400); // 207 Multi-Status
-            echo json_encode(["deleted" => $deletedIds, "errors" => $errors]);
-        } else {
-            http_response_code(200);
-            echo json_encode(["message" => "Products deleted", "deleted" => $deletedIds]);
-        }
-        break;
-        }
-
-
-
 
     default:
         http_response_code(405);
